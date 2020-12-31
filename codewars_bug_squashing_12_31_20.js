2fd525376db2f7bbde3eb6d914ba7aa241ebe560
Crew Spence <csspence123@gmail.com>
2020-12-31 08:57:16 -0600

    Wrote working solution.

--- codewars_bug_squashing_12_31_20.js
+++ codewars_bug_squashing_12_31_20.js
@@ -16,11 +16,11 @@ var health = 100
 var position = 0
 var coins = 0
 
-function main () {
-  getCoins()
-  move()
-  printStatus()
-  combat()
-  rolDice()
-  attack()
+const main = () => {
+  rollDice();
+  move();
+  combat();
+  getCoins();
+  buyHealth();
+  printStatus();
 }
